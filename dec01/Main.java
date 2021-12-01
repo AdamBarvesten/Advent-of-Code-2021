231deb0b5f514a0d3946cd543b4e1922b47a0969
Adam Barvesten <ABARVEST@volvocars.com>
2021-12-01 15:48:48 +0100

    Major Refactorisation, combine 2 methods to one

--- dec01/Main.java
+++ dec01/Main.java
@@ -5,28 +5,17 @@ import java.nio.file.Paths;
 public class Main {
     public static void main(String[] args) throws IOException {
         int[] input = Files.readAllLines(Paths.get("dec01/input.txt")).stream().mapToInt(Integer::parseInt).toArray();
-        
-        part1(input);
-        part2(input);
+        solve(input,1);
+        solve(input,3);
     }
     
-    private static void part1(int[] in){
+    private static void solve(int[] in, int window){
         int counter = 0;
-        for(int i=0 ; i<in.length-1 ; i++){
-            if(in[i+1] > in[i]){
+        for(int i=0 ; i<in.length-window ; i++){
+            if(in[i+window] > in[i]){
                 counter++;
             }
         }
-        System.out.println("Part one: " + counter);
-    }
-
-    private static void part2(int[] in){
-        int counter = 0;
-        for(int i=0; i<in.length-3; i++){
-                if(in[i+3] > in[i]){
-                    counter++;
-                }
-        }
-        System.out.println("Part two: " + counter);
+        System.out.println("Solution with window " + window + ": " + counter);
     }
 }
