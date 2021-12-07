892a45b4ca1cef2f94aed5e2fb24dab1ae527ea2
Adam Barvesten <ABARVEST@volvocars.com>
2021-12-07 13:38:48 +0100

    fix of input bug dec04

--- dec04/Dec04.java
+++ dec04/Dec04.java
@@ -13,22 +13,23 @@ public class Dec04 {
     public static void main(String[] args) throws IOException {
         File file = new File("dec04/input.txt");
         Scanner scanner = new Scanner(file);
+
         int [] numbers = Stream.of(scanner.next().split(",")).mapToInt(Integer::parseInt).toArray();
-        scanner.close();
-        
         int[][] board = new int[5][5];
         int index = 0;
 
         scanner.nextLine();
-        while(scanner.hasNextLine()){
+        while(scanner.hasNextInt()){
             for(int i = 0; i < 5; i++){
                 for(int j = 0; j<5 ; j++){
                     board[i][j] = scanner.nextInt();
                 }
             }
+            printBoard(board);
             boards.add(board);
             board = new int[5][5];
         }
+        scanner.close();
 
         while(winScore==0 || looseScore==0){
             drawNumber(numbers[index]);
@@ -37,6 +38,16 @@ public class Dec04 {
 
         System.out.println("Part One: " + winScore);
         System.out.println("Part Two: " + looseScore);
+    }
+
+    private static void printBoard(int[][] board) {
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board.length; j++) {
+                System.out.print(board[i][j] + " ");
+            }
+            System.out.println("");
+        }
+        System.out.println("");
     }
 
     private static int calculateBoard(int[][] board) {
@@ -83,4 +94,4 @@ public class Dec04 {
         }
         return false;
     }
-}
+} 
