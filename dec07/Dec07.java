1cab6979140c236197189d11c70ae51ec179caf4
Adam Barvesten <ABARVEST@volvocars.com>
2021-12-07 10:54:00 +0100

    solution dec07

--- dec07/Dec07.java
+++ dec07/Dec07.java
@@ -1,49 +1,21 @@
-import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.Arrays;
-import java.util.List;
-import java.util.Scanner;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class Dec07 {
+    public static void main(String[] args) throws IOException {
+        int [] crabs = Arrays.stream(Files.readString(Path.of("dec07/input.txt")).trim().split(",")).mapToInt(Integer::parseInt).sorted().toArray();
+        double median = (crabs.length % 2 == 0) ? ((double)crabs[crabs.length/2] + (double)crabs[crabs.length/2 - 1])/2 : (double)crabs[crabs.length/2];
+        double mean = Math.floor(Arrays.stream(crabs).average().orElse(Double.NaN));
 
-    public static void main(String[] args) throws FileNotFoundException {
-        File file = new File("dec07/input.txt");
-        Scanner scanner = new Scanner(file);
-
-        int [] pos = Stream.of(scanner.next().split(",")).mapToInt(Integer::parseInt).toArray();
-        Arrays.sort(pos);
-        double median = (pos.length % 2 == 0) ? ((double)pos[pos.length/2] + (double)pos[pos.length/2 - 1])/2 : (double)pos[pos.length/2];
-
-        double mean = 0;
-
-
-        for (int po : pos) { mean += po;}
-        mean = mean / pos.length;
-        mean = Math.round(Arrays.stream(pos).average().orElse(Double.NaN));
-        System.out.println(mean);
-
-        int sum = 0;
-        for(int p : pos){
-            double dist = Math.abs(p-mean);
-            sum +=  dist * ( dist + 1 ) / 2;
+        int fuel1 = 0;
+        int fuel2 = 0;
+        for(int c : crabs){
+            fuel1 += Math.abs(c - median);
+            fuel2 += Math.abs(c-mean) * ( Math.abs(c-mean) + 1 ) / 2;
         }
 
-
-        System.out.println("fuel: "+calculateFuel(pos, median));
-        System.out.println("fuel2: " + sum);
-
+        System.out.println("Part One: " + fuel1 + "\n" + "Part Two: " + fuel2);
     }
-
-    private static int calculateFuel(int[] pos, double median) {
-        int sum = 0;
-        for(int p : pos){
-            sum += Math.abs(p - median);
-        }
-        return sum;
-    }
-
-    //too high 101268141
-
 }
