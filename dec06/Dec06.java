6cf7d4d51079fd5ab45b61e3ba6fadcfc4d4aae8
Adam Barvesten <ABARVEST@volvocars.com>
2021-12-07 11:28:31 +0100

    code cleanup dec06

--- dec06/Dec06.java
+++ dec06/Dec06.java
@@ -7,30 +7,26 @@ public class Dec06 {
     public static void main(String[] args) throws IOException {
         File file = new File("dec06/input.txt");
         Scanner scanner = new Scanner(file);
-
         List<Integer> numbers = Arrays.stream(scanner.next().split(",")).map(Integer::valueOf).collect(Collectors.toList());
         scanner.close();
         
-        System.out.println("Day 80: " + solve(80, numbers));
-        System.out.println("Day 256: " + solve(256, numbers));
-
+        System.out.println("Day 80: " + solve(80, numbers) + "\n" + "Day 256: " + solve(256, numbers));
     }
 
     private static long solve(int day, List<Integer> numbers){
         long[] fishes = new long[9];
+        for (int n : numbers) {fishes[n]++;}
 
-        for (int i : numbers) {
-            fishes[i]++;
-        }
-
-        for (int i = 1 ; i <= day; i++) {
+        while(day > 0){
             long spawningFish = fishes[0];
             for (int j = 0 ; j < fishes.length-1; j++) {
                 fishes[j] = fishes[j+1];
             }
-            fishes[6] += spawningFish;
             fishes[8] = spawningFish;
+            fishes[6] += spawningFish;
+            day--;
         }
+
         return Arrays.stream(fishes).sum();
     }
 }
